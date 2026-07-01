92962e1ba66e9a0d3414e86473f9649edee95350
Jieson DelaFuente <jiesonleobendelafuente@gmail.com>
2026-07-01 20:47:45 +0800

    feat: add addition program that computes sum of two numbers

---
+++ addition.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class addition {
+
+    public static void main(String[] args){
+
+        int num1, num2;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter First Number: ");
+        num1 = scanner.nextInt();
+        System.out.println("Enter Second Number: ");
+        num2 = scanner.nextInt();
+        System.out.println("Sum: " + (num1 + num2));
+
+    }
+
+}
